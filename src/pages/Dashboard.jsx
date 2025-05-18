c0c5d52913f58088d44d44b46594509101cae57c
Nitinjr812 <nitinjr812@gmail.com>
2025-05-18 17:39:11 +0530

    layout done

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -305,7 +305,7 @@ const Dashboard = () => {
       <div className="max-w-5xl mx-auto">
         <div className="flex flex-col md:flex-row justify-between items-center mb-8">
           <div className="flex items-center mb-4 md:mb-0">
-            <img className='h-15 w-15 mt-1' src="/fawicon (2).png" alt="" />
+            <img className='h-20 w-20 ' src="/fawicon (2).png" alt="" />
             <h1 className="text-3xl font-bold text-white tracking-tight">Cryvix</h1>
           </div>
           
@@ -587,7 +587,7 @@ const Dashboard = () => {
       </div>
       <button
             onClick={handleLogout}
-            className="px-4 py-2 bg-red-600 hover:bg-red-700 text-white rounded-lg transition-all duration-200 flex items-center shadow-lg"
+            className="px-4 mt-9 m-auto py-2 bg-red-600 hover:bg-red-700 text-white rounded-lg transition-all duration-200 flex items-center shadow-lg"
           >
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clipRule="evenodd" />
