09dd26f8d1f952d0998dd02a623a2c50526593d2
Nitinjr812 <nitinjr812@gmail.com>
2025-05-18 17:36:53 +0530

    api fixesss

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -308,15 +308,7 @@ const Dashboard = () => {
             <img className='h-15 w-15 mt-1' src="/fawicon (2).png" alt="" />
             <h1 className="text-3xl font-bold text-white tracking-tight">Cryvix</h1>
           </div>
-          <button
-            onClick={handleLogout}
-            className="px-4 py-2 bg-red-600 hover:bg-red-700 text-white rounded-lg transition-all duration-200 flex items-center shadow-lg"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clipRule="evenodd" />
-            </svg>
-            Logout
-          </button>
+          
         </div>
 
         <div className="bg-gray-800 bg-opacity-50 rounded-xl p-6 md:p-8 shadow-2xl backdrop-blur-sm mb-6 border border-gray-700">
@@ -593,6 +585,15 @@ const Dashboard = () => {
           </div>
         )}
       </div>
+      <button
+            onClick={handleLogout}
+            className="px-4 py-2 bg-red-600 hover:bg-red-700 text-white rounded-lg transition-all duration-200 flex items-center shadow-lg"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+              <path fillRule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clipRule="evenodd" />
+            </svg>
+            Logout
+          </button>
     </div>
   );
 };
