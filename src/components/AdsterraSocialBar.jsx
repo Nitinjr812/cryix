a5c8c0daa58a77624c089f8f585eb27623b27cd6
Nitinjr812 <nitinjr812@gmail.com>
2025-05-22 12:06:01 +0530

    google

---
+++ src/components/AdsterraSocialBar.jsx
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+
+function AdsterraSocialBar() {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = "//pl26710091.profitableratecpm.com/e9/b7/d6/e9b7d6616a09b75578706430aed69b97.js";
+        script.type = "text/javascript";
+        script.async = true;
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
+    return null;
+}
+
+export default AdsterraSocialBar;
