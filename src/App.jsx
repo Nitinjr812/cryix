df221b7b3d22d13c9422056192e7b0d413d89193
Nitinjr812 <nitinjr812@gmail.com>
2025-05-22 13:13:23 +0530

    google

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-// import AdsterraAds from "./components/AdsterraAds";
+import AdsterraAds from "./components/AdsterraAds";
 import AdsterraSocialBar from "./components/AdsterraSocialBar";
 import Approutes from "./routes/Approutes";
 import 'react-toastify/dist/ReactToastify.css';
 export default function App() {
   return (
     <>
-      {/* <AdsterraAds /> */}
+      <AdsterraAds />
       <AdsterraSocialBar />
       <Approutes />
     </>
